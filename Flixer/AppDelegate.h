55dde71f394453730af73ce2ca4892162bf03718
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-06-26 10:59:36 -0700

    Initial Commit

---
+++ Flixer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flixer
+//
+//  Created by gilemos on 6/26/19.
+//  Copyright © 2019 gilemos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flixer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flixer
+//
+//  Created by gilemos on 6/26/19.
+//  Copyright © 2019 gilemos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
